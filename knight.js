3906f5ce15952740e1116404ecbfeb76b2ab80f2
Chris Harding <chrisharding.m@gmail.com>
2021-03-05 12:54:57 -0700

    Fixed an error in the code.
    One of the numbers needed to be negative. Corrected this error.

--- knight.js
+++ knight.js
@@ -1,3 +1,8 @@
+//Script by Chris Harding.
+//This script will calculate any available move positions for a knight piece.
+//There are 8 rows and 8 columns, the rows will be X and the columns Y (x,y).
+//This script Assumes that the position (0,0) starts in the bottom left corner.
+
 //This variable represents the Knights starting position.
 //You can change this variable and get different results from the program.
 //The first number is the knights X position and the second number is the Y position.
@@ -5,7 +10,7 @@ var knightPosition = [1, 2];
 console.log("The knights current position is", knightPosition)
 
 
-//This is a variable for the final possible positions the knight can move to.
+//This is an array for the final possible positions the knight can move to.
 var allPossibleMoves = []
 
 //This is a function that will take the knights current position and apply new X and Y values to create a new position.
@@ -21,12 +26,12 @@ function calculateMoves (x,y) {
   }
 }
 
-//The Knigth has eight possible moves when near the center of the board. 
+//The Knight has a total of eight possible moves. 
 //The calculatemoves function will need to run eight times to check for all possibilities.
 calculateMoves (2,1);
 calculateMoves (1,2);
 calculateMoves (-2,-1);
-calculateMoves (1,-2);
+calculateMoves (-1,-2);
 
 calculateMoves (2,-1);
 calculateMoves (-1,2);
@@ -37,6 +42,6 @@ calculateMoves (1,-2);
 console.log("The knight can move to the following positions")
 console.log(allPossibleMoves)
 
-//I chose to do this method because I wanted to make it as simple as I could without a lot of unnecessary code.
+//I decided to do this method because I wanted to make it as simple as I could without a lot of unnecessary code.
 //Creating a function to handle the calculation helped with this since the command can be called more than once.
 //Any code that repeats more than once should ideally be put into a function to help clean things up. 
